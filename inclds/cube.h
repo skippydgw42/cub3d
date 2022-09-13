08e3d1a65785d80299bd7845abc5d768569abf58
Martin Degraeuwe <mdegraeu@z4r6p3.42lyon.fr>
2022-09-13 14:18:05 +0200

    notes before start

---
+++ inclds/cube.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdegraeu <mdegraeu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 12:20:09 by mdegraeu          #+#    #+#             */
+/*   Updated: 2022/09/13 13:55:44 by mdegraeu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+# define CUBE_H
+
+# include "../mlib/mlx.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <math.h>
+
+# define WIDTH
+# define HEIGHT
+
+#endif
